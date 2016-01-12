db31c62203c3e601a32d505e1abdf10694cfda9e
ekkasithw <ekkasith_w@hotmail.com>
2016-01-13 00:13:42 +0700

    INIT

--- assets/directives/sortable.js
+++ assets/directives/sortable.js
@@ -4,41 +4,6 @@
 
 angular.module('tripPlanner')
   .directive('sortable', function($http) {
-    function parseParamStr(paramsStr) {
-      if (typeof str !== 'string') {
-        return {};
-      }
-
-      str = str.trim().replace(/^(\?|#|&)/, '');
-
-      if (!str) {
-        return {};
-      }
-
-      return str.split('&').reduce(function (ret, param) {
-        var parts = param.replace(/\+/g, ' ').split('=');
-        // Firefox (pre 40) decodes `%3D` to `=`
-        // https://github.com/sindresorhus/query-string/pull/37
-        var key = parts.shift();
-        var val = parts.length > 0 ? parts.join('=') : undefined;
-
-        key = decodeURIComponent(key);
-
-        // missing `=` should be `null`:
-        // http://w3.org/TR/2012/WD-url-20120524/#collect-url-parameters
-        val = val === undefined ? null : decodeURIComponent(val);
-
-        if (!ret.hasOwnProperty(key)) {
-          ret[key] = val;
-        } else if (Array.isArray(ret[key])) {
-          ret[key].push(val);
-        } else {
-          ret[key] = [ret[key], val];
-        }
-
-        return ret;
-      }, {});
-    }
 
     return {
         restrict: 'AC',
@@ -65,7 +30,7 @@ angular.module('tripPlanner')
                 ui.item.attr('id', 'item-' + ui.item.attr('data-id'));
               }
 
-              var data = $(this).sortable('serialize');
+              var data = $.deparam($(this).sortable('serialize'));
 
               $http.post(attrs['itemOrderUpdateUrl'], data).success(function(result) {});
             }
